3a93808c3c21660b157d268ca1478bd3ee2aadf0
punkbibi <alexandre.bibilashvili.1@btu.edu.ge>
2024-12-14 15:04:43 +0400

    changed it into falsk app

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, render_template
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
@@ -5,13 +6,7 @@ import random
 import os
 import sqlite3
 
-conn = sqlite3.connect('spotify.db')
-cursor = conn.cursor()
-
-# create a table to store the songs if it doesn't exist
-cursor.execute('''CREATE TABLE IF NOT EXISTS songs
-                (name TEXT, artist TEXT, url TEXT)''')
-
+app = Flask(__name__)
 
 # Load the environment variables
 load_dotenv()
@@ -25,74 +20,60 @@ def get_all_tracks(query, search_type='track', limit=50):
     filtered_tracks = []
 
     while True:
-        # search for the tracks
         results = sp.search(q=query, type=search_type, limit=limit, offset=offset)
-        
-        # get the tracks and the total number of tracks
         tracks = results['tracks']['items']
         total_tracks = results['tracks']['total']
 
-        # loop through the tracks and filter the tracks based on the query
         for track in tracks:
-            for artist in track['artists']: # loop through the artists of the track
+            for artist in track['artists']:
                 if query.lower() in artist['name'].lower():
-                    filtered_tracks.append(track)
-                    break # break the loop if the artist is found
-                
-    
+                    filtered_tracks.append({
+                        'name': track['name'],
+                        'artist': artist['name'],
+                        'url': track['external_urls']['spotify']
+                    })
+                    break
+
         offset += limit
         if offset >= total_tracks:
             break
-        
     return filtered_tracks
-
 
 def get_random_song(tracks):
     if not tracks:
         return None
-    
-    random_track = random.choice(tracks)
-    # return the name, artist and the url of the random track in a dictionary
-    return {
-        'name': random_track['name'],
-        'artist': random_track['artists'][0]['name'],
-        'url': random_track['external_urls']['spotify']
-    }
+    return random.choice(tracks)
 
-def main():
-    while True:
-        #TODO: add a function to search with genre
-        
-        query = input("Enter a artist: ")
-        
-        tracks = get_all_tracks(query)
-        song = get_random_song(tracks)
-        
-        existing_songs = cursor.execute("SELECT name, artist FROM songs").fetchall()
-        
-        print(f"Number of songs found: {len(tracks)}")    
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        # check if the song is already in the database
-        if (song['name'], song['artist']) in existing_songs:
-            print("This song has already been added to the database.")
-            continue
-        
-        elif song:
-            print(f"Random Song: {song['name']} by {song['artist']}")
-            print(f"Listen here: {song['url']}")
-            
-            # add the song to the database
-            cursor.execute("INSERT INTO songs VALUES (?, ?, ?)", (song['name'], song['artist'], song['url']))
-            conn.commit()
-            
-            print("Song added to the database.")
-            break
-        else:
-            print("No songs found for the given query.")
-            break
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    tracks = get_all_tracks(query)
+    if not tracks:
+        return render_template('results.html', message="No songs found for the given query.")
 
+    song = get_random_song(tracks)
 
-if __name__ == "__main__":
-    main()
+    conn = sqlite3.connect('spotify.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS songs
+                    (name TEXT, artist TEXT, url TEXT)''')
+
+    existing_songs = cursor.execute("SELECT name, artist FROM songs").fetchall()
+    if (song['name'], song['artist']) in existing_songs:
+        cursor.close()
+        conn.close()
+        return render_template('results.html', message="This song has already been added to the database.", song=song)
+
+    cursor.execute("INSERT INTO songs VALUES (?, ?, ?)", (song['name'], song['artist'], song['url']))
+    conn.commit()
     cursor.close()
     conn.close()
+
+    return render_template('results.html', message="Song added to the database.", song=song)
+
+if __name__ == "__main__":
+    app.run()
